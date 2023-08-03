089b5db23d919e4323b6ca468bda66d3082c7a49
Zephyrusa16 <140947644+Zephyrusa16@users.noreply.github.com>
2023-08-03 15:29:42 +0700

    index.html and friends

--- contact.php
+++ contact.php
@@ -1,19 +1,43 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
+// Jika menggunakan PHPMailer, pastikan Anda mengatur jalur ke direktori PHPMailer
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
 
-  // Lakukan pemrosesan data di sini (misalnya: kirim email, simpan ke database, dll.)
+// Konfigurasi email Anda
+$senderEmail = 'contoh@gmail.com'; // Email pengirim
+$senderPassword = 'password_pengirim'; // Password email pengirim
+$receiverEmail = 'email_anda@gmail.com'; // Email penerima
 
-  // Contoh: Kirim email
-  $to = "elevatesphere16@gmail.com";
-  $subject = "Pesan dari " . $name;
-  $headers = "From: " . $email . "\r\n";
-  mail($to, $subject, $message, $headers);
+// Tangkap data dari form kontak
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
 
-  // Redirect pengguna kembali ke halaman awal setelah pengiriman
-  header("Location: index.html");
-  exit();
+// Buat objek PHPMailer
+$mail = new PHPMailer();
+
+// Set penggunaan SMTP
+$mail->isSMTP();
+$mail->Host = 'elevatesphere16.gmail.com'; // Ganti dengan server SMTP Anda jika tidak menggunakan Gmail
+$mail->SMTPAuth = true;
+$mail->Username = $senderEmail;
+$mail->Password = $senderPassword;
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+
+// Set penerima email
+$mail->setFrom($senderEmail, $name);
+$mail->addAddress($receiverEmail);
+
+// Set email pesan
+$mail->isHTML(true);
+$mail->Subject = 'Pesan dari Form Kontak';
+$mail->Body = "<p>Nama: $name</p><p>Email: $email</p><p>Pesan: $message</p>";
+
+// Kirim email
+if ($mail->send()) {
+    echo 'Pesan telah terkirim! Terima kasih atas pesannya.';
+} else {
+    echo 'Pesan gagal terkirim. Silakan coba lagi.';
 }
 ?>
